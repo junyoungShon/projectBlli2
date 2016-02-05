fd41d6e39897bc970f0e0c35b530936a78cadf69
Park <Park@192.168.25.10>
2016-02-05 19:06:00 +0900

    asd

--- projectBlli2/src/main/java/kr/co/blli/controller/BlliInterceptor.java
+++ projectBlli2/src/main/java/kr/co/blli/controller/BlliInterceptor.java
@@ -2,11 +2,7 @@ package kr.co.blli.controller;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.context.SecurityContext;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 public class BlliInterceptor extends HandlerInterceptorAdapter{
--- projectBlli2/src/main/java/kr/co/blli/controller/MemberController.java
+++ projectBlli2/src/main/java/kr/co/blli/controller/MemberController.java
@@ -32,6 +32,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
